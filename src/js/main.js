16b7f028a86640d67363362a203a975f0d661bab
Alex Burgos <alexander.burgos1@gmail.com>
2016-07-26 15:44:55 -0400

    removed unused folders/deps

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,7 @@
-import $ from 'jquery';
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './components';
 
 import '../styles/main.scss';
 
-const mount = document.getElementById('app-mount');
-
-$(() => {
-  render(<App/>, mount);
-  }
-)
+render(<App/>, document.querySelector("#react-mount"));
